8c595ef4423ffca6123648341f6271b2418e9186
omerfarukkaratug <115614928+omerfarukkaratug@users.noreply.github.com>
2026-09-01 15:01:36 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'sayim-v39';
+const CACHE = 'sayim-v40';
 const FILES = ['./manifest.json', './icon.svg', './icon-192.png', './icon-512.png', './icon-512-maskable.png', './logo-header.png'];
 
 self.addEventListener('install', (e) => {
